0f91fc233e7245e5b286b85e01134c17822e9a1b
RafaelRRcruz <164397283+RafaelRRcruz@users.noreply.github.com>
2024-04-06 17:43:39 -0300

    rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,1 +1,11 @@
 
+import pagamento from "./../pages/pagamento/pagamento";
+import planos from "./../pages/planos/planos";
+
+
+export const rootReducer = {
+    pagamento,
+    planos
+}
+
+export default rootReducer;
