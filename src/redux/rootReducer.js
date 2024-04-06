9544a7066ebe098d54d7290d3394a1789ea1a609
derio123 <deriomatheus6@gmail.com>
2024-04-06 17:12:06 -0300

    Add o reducer e redux

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,10 @@
+import pagamentos from "./../pages/pagamentos/pagamentos";
+import planos from "./../pages/planos/planos";
+
+
+export const rootReducer = {
+    pagamentos,
+    planos
+}
+
+export default rootReducer;
