257886849aef6dfe64fcef17ed0f0b45b2681881
RafaelRRcruz <164397283+RafaelRRcruz@users.noreply.github.com>
2024-04-08 22:05:01 -0300

    Delete src/redux directory

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-const persistedReducer = combineReducers({
-    ...rootReducer
-  });
-
-export const store = configureStore({
-    reducer: persistedReducer,
-});
