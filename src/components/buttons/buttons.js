0f420fd422309be456c6dfbee5280199227d83f9
derio123 <deriomatheus6@gmail.com>
2024-04-15 15:02:23 -0300

    Ajuste no butão de pagamento

--- src/components/buttons/buttons.js
+++ src/components/buttons/buttons.js
@@ -2,7 +2,7 @@ import { Button } from 'react-bootstrap';
 
 export function Buttons(params) {
     return (
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" className='mb-2 mt-2'>
             Pagar
         </Button>
     );
