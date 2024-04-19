13cc4807860093a44653d95c7f4aeb8debc793ff
RafaelRRcruz <164397283+RafaelRRcruz@users.noreply.github.com>
2024-04-19 19:59:24 -0300

    home.js

---
+++ src/Pages/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Pesquisa from '../../components/pesquisa/pesquisa';
+
+export function Home() {
+
+  return (
+    <div className="App">
+     <h1>Bem vindo</h1>
+     <Pesquisa />
+
+    </div>
+  );
+}
+
+export default Home;
