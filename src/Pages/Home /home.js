d67087363c466d91ad2313192a30f67fb9724069
RafaelRRcruz <164397283+RafaelRRcruz@users.noreply.github.com>
2024-04-21 19:54:17 -0300

    Add tela de pagamento

---
+++ src/Pages/Home /home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FerramentaPesquisa from '../../Components/FerramentaPesquisa/pesquisa';
+
+export function Home() {
+
+  return (
+    <div className="App">
+     <h1>Bem vindo</h1>
+     <FerramentaPesquisa />
+
+    </div>
+  );
+}
+
+export default Home;
