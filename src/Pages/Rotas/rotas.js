b0fa2c809646c2736f31deb9c55c0198facb9ce4
RafaelRRcruz <164397283+RafaelRRcruz@users.noreply.github.com>
2024-04-21 19:53:15 -0300

    Add tela de pagamento

--- src/Pages/Rotas/rotas.js
+++ src/Pages/Rotas/rotas.js
@@ -6,11 +6,17 @@ import FAQ from '../FAQ/index';
 import Contatos from '../Contatos/index';
 import Login from '../Login/Login'
 import Alugar from '../Aluguel';
+import Planos from '../Planos/planos';
+import Footer from "../../Components/Footer/footer";
+import HeaderMain from "../../Components/Header";
+import Historico from "../Pagamento/historico";
+import Home from "../Home/home";
 import{ BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export const AppRouter =() => {
     return(
         <Router>
+            <HeaderMain />
     <div>
         <nav>
             <ul>
@@ -19,7 +25,6 @@ export const AppRouter =() => {
             </ul>
         </nav>
               <Switch>
-              <Route path="/pagamento"><Pagamento /></Route>
               <Route path={`/detalhes/:id`}><Detalhes /></Route>
               <Route path="/sobre-nos"><SobreNos/></Route>
               <Route path="/FAQ"><FAQ/></Route>
@@ -27,9 +32,13 @@ export const AppRouter =() => {
               <Route path="/login"><Login /></Route>
               <Route path={`/Aluguel/:id`}><Alugar/></Route>
               <Route path="/"><Principal /></Route>
-
+              <Route path="/home"><Home /></Route>
+              <Route path="/pagamento"><Pagamento /></Route>
+              <Route path="/planos"><Planos /></Route>
+              <Route path="/historico"><Historico /></Route>
               </Switch>
       </div>
+      <Footer />
   </Router> 
     );
 }
