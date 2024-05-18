8a700bafa6f0da8ea08eaf0c444240dd0248a326
RafaelRRcruz <164397283+RafaelRRcruz@users.noreply.github.com>
2024-05-18 20:10:01 -0300

    Mudanças

---
+++ src/data/data.js
@@ -0,0 +1,123 @@
+
+export const carrosData = [
+    {
+        "id": 1,
+        "marca": "Ferrari",
+        "modelo": "Purosangue",
+        "ano": 2022,
+        "cor": "Azul",
+        "preco": 400.00,
+        "img": require('../../src/assets/Ferrari.jpeg'),
+        "detalhe": "Direção hidráulica, Rádio e bluetooth, , consumo médio 18km/l, Mini TV ",
+        "diasAlugado": [
+            "10/06/2024", "11/06/2024", "12/06/2024", "13/06/2024", "14/06/2024",
+            "16/05/2024", "18/05/2024", "22/05/2024", "25/05/2024", "27/05/2024",
+            "02/06/2024", "05/06/2024", "07/06/2024", "16/06/2024", "20/06/2024"
+        ]
+    },
+    {
+        "id": 2,
+        "marca": "Honda",
+        "modelo": "Civic",
+        "ano": 2019,
+        "cor": "Preto",
+        "preco": 280.00,
+        "img": require('../../src/assets/honda.jpg'),
+        "detalhe": "Direção hidráulica, Rádio e bluetooth, , consumo médio 13km/l e câmbio manual",
+        "diasAlugado": [
+            "10/06/2024", "11/06/2024", "12/06/2024", "13/06/2024", "14/06/2024",
+            "16/05/2024", "18/05/2024", "22/05/2024", "25/05/2024", "27/05/2024",
+            "02/06/2024", "05/06/2024", "07/06/2024", "16/06/2024", "20/06/2024"
+        ]
+    },
+    {
+        "id": 3,
+        "marca": "Ford",
+        "modelo": "KA",
+        "ano": 2018,
+        "cor": "Vermelho",
+        "preco": 270.00,
+        "img": require('../../src/assets/FordKa.jpg'),
+        "detalhe": "Direção hidráulica, Rádio e bluetooth, , consumo médio 13km/l e câmbio manual",
+        "diasAlugado": [
+            "10/06/2024", "11/06/2024", "12/06/2024", "13/06/2024", "14/06/2024",
+            "16/05/2024", "18/05/2024", "22/05/2024", "25/05/2024", "27/05/2024",
+            "02/06/2024", "05/06/2024", "07/06/2024", "16/06/2024", "20/06/2024"
+        ]
+    },
+    {
+        "id": 4,
+        "marca": "Mercedes",
+        "modelo": "AMG GTT",
+        "ano": 2021,
+        "cor": "Cinza",
+        "preco": 400.00,
+        "img": require('../../src/assets/Mercedes.jpg'),
+        "detalhe": "Direção hidráulica, Rádio e bluetooth, , consumo médio 15km/l ",
+        "diasAlugado": [
+            "10/06/2024", "11/06/2024", "12/06/2024", "13/06/2024", "14/06/2024",
+            "16/05/2024", "18/05/2024", "22/05/2024", "25/05/2024", "27/05/2024",
+            "02/06/2024", "05/06/2024", "07/06/2024", "16/06/2024", "20/06/2024"
+        ]
+    },
+    {
+        "id": 5,
+        "marca": "Tesla",
+        "modelo": "Roadster",
+        "ano": 2022,
+        "cor": "Cinza Claro",
+        "preco": 400.00,
+        "img": require('../../src/assets/Tesla.jpg'),
+        "detalhe": "Direção hidráulica, Rádio e bluetooth, , consumo médio 18km/l e Mini TV",
+        "diasAlugado": [
+            "10/06/2024", "11/06/2024", "12/06/2024", "13/06/2024", "14/06/2024",
+            "16/05/2024", "18/05/2024", "22/05/2024", "25/05/2024", "27/05/2024",
+            "02/06/2024", "05/06/2024", "07/06/2024", "16/06/2024", "20/06/2024"
+        ]
+    },
+    {
+        "id": 6,
+        "img": require('../../src/assets/fiat-uno.jpg'),
+        "img2": require('../../src/assets/Fiat-uno2.jpg'),
+        "modelo": "Fiat Uno",
+        "ano": "2014",
+        "dono": "João da Silva",
+        "cidade": "Rio de Janeiro, RJ",
+        "preco": 120.00,
+        "detalhe": "Direção hidráulica, Rádio e bluetooth, , consumo médio 13km/l e câmbio manual",
+        "diasAlugado": [
+            "10/06/2024", "11/06/2024", "12/06/2024", "13/06/2024", "14/06/2024",
+            "16/05/2024", "18/05/2024", "22/05/2024", "25/05/2024", "27/05/2024",
+            "02/06/2024", "05/06/2024", "07/06/2024", "16/06/2024", "20/06/2024"
+        ]
+    },
+
+    {
+        "img": require('../../src/assets/FordKa.jpg'),
+        "img2": require('../../src/assets/FordKa2.jpg'),
+        "modelo": "Ford Ka",
+        "ano": "2017",
+        "dono": "João da Silva",
+        "cidade": "Mogi das Cruzes, SP",
+        "preco": 180.00,
+        "detalhe": "Direção Elétrica, rádio e bluetooth, motor 1.0, câmbio manual e consumo médio de 14km/l",
+        "diasAlugado": [
+            "10/06/2024", "11/06/2024", "12/06/2024", "13/06/2024", "14/06/2024",
+            "16/05/2024", "18/05/2024", "22/05/2024", "25/05/2024", "27/05/2024",
+            "02/06/2024", "05/06/2024", "07/06/2024", "16/06/2024", "20/06/2024"
+        ]
+    }
+];
+
+
+export const clientesData = [
+    {
+        "id": "1",
+        "nome": "Ana Ferreira Azevedo",
+        "cpf": "09168846193",
+        "telefone": "21973434003",
+        "endereco": [{"rua": "",  "numero": "", "estado": ""}],
+        "dataNasc": "01/12/1992",
+        "cnh":"23215858290"
+    }
+];
