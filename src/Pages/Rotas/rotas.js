135c98622d912396310f01684cb5d116093d78c0
RafaelRRcruz <164397283+RafaelRRcruz@users.noreply.github.com>
2024-05-22 18:18:09 -0300

    Mudanças

--- src/Pages/Rotas/rotas.js
+++ src/Pages/Rotas/rotas.js
@@ -1,51 +1,41 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Pagamento from '../Pagamento/Pagamento';
-import Planos from '../Planos/planos';
-import Footer from "../../Components/Footer/footer";
-import HeaderMain from "../../Components/Header/index";
-import Historico from '../Pagamento/historico';
-
-import Forms from "../../components/forms/forms";
-import Principal from '../Principal/Principal';
-import FAQ from '../FAQ/index';
-import Criarcarro from "../CriarCarro";
-import Atualizarcarro from '../Atualizarcarro/Atualizarcarro';
-import Atualizardadoscarro from '../Atualizardadoscarro/Atualizardadoscarro'
-import Contatos from '../Contatos/index';
 import Detalhes from '../Detalhes/Detalhes';
+import Pagamento from '../Pagamento/Pagamento';
+import Principal from '../Principal/Principal';
+import Atualizarcarro from '../Atualizarcarro/Atualizarcarro';
+import FAQ from '../FAQ/index';
+import Contatos from '../Contatos/index';
+import Login from '../Login/Login';
 import Alugar from '../Aluguel';
+import Criarcarro from '../Criarcarro/Criarcarro';
+import{ BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import HistoricoDetalhes from '../Pagamento/historicoDetalhes';
-import Clientes from "../clientes";
-import AtualizarDadosClientes from "../atualizarDadosClientes/atualizarDadosClientes";
-import CadastrarClientes from "../cadastrarClientes/cadastrarClientes";
-import AtualizarClientes from "../atualizarClientes/atualizarClientes";
+import Historico from '../Pagamento/historico';
+import Atualizardadoscarro from '../Atualizardadoscarro/Atualizardadoscarro'
 
-export const AppRouter = () => {
-    return (
+
+
+export const AppRouter =() => {
+    return(
         <Router>
-            <HeaderMain />
-            <div>
-                <Routes>
-                    <Route exact path="/" Component={Principal}></Route>
-                    <Route path="/pagamentos" Component={Pagamento}></Route>
-                    <Route path="/planos" Component={Planos}></Route>
-                    <Route path="/historico" Component={Historico}></Route>
-                    <Route path="/forms" Component={Forms}></Route>
-                    <Route path="/faq" Component={FAQ}></Route>
-                    <Route path="/criarcarro" Component={Criarcarro}></Route>
-                    <Route path="/registar" Component={CadastrarClientes}></Route>
-                    <Route path="/atualizarcarro" Component={Atualizarcarro}></Route>
-                    <Route path={`/atualizardadoscarro/:id`} Component={Atualizardadoscarro}></Route>
-                    <Route path="/contatos" Component={Contatos}></Route>
-                    <Route path={`/detalhes/:id`} Component={Detalhes}></Route>
-                    <Route path={`/aluguel/:id`} Component={Alugar}></Route>
-                    <Route path={"/historico/:index"} Component={HistoricoDetalhes}></Route>
-                    <Route path="/listarcliente" Component={Clientes}></Route>
-                    <Route path="/atualizarcliente" Component={AtualizarClientes}></Route>
-                    <Route path={`/atualizardadoscliente/:id`} Component={AtualizarDadosClientes}></Route>
-                </Routes>
-            </div>
-            <Footer />
-        </Router>
+    <div>
+
+              <Switch>
+              <Route path="/pagamento"><Pagamento /></Route>
+              <Route path={`/detalhes/:id`}><Detalhes /></Route>
+              <Route path="/atualizarcarro"><Atualizarcarro/></Route>
+              <Route path="/FAQ"><FAQ/></Route>
+              <Route path="/contatos"><Contatos/></Route>
+              <Route path="/login"><Login /></Route>
+              <Route path={`/Aluguel/:id`}><Alugar/></Route>
+              <Route path="/criarcarro"><Criarcarro /></Route>
+              <Route path={"/historico/:index"}><HistoricoDetalhes /></Route>
+              <Route path="/historico"><Historico /></Route>
+              <Route path={"/historico/:index"}><Historico /></Route>
+              <Route path="/atualizardadoscarro/:id"><Atualizardadoscarro/></Route>
+              <Route path="/"><Principal /></Route>
+
+              </Switch>
+      </div>
+  </Router> 
     );
 }
