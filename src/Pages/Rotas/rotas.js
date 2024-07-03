8441dd972224900d096b2dec020b9b5dee8a6670
RafaelRRcruz <164397283+RafaelRRcruz@users.noreply.github.com>
2024-07-03 17:34:13 -0300

    Mudanças

--- src/Pages/Rotas/rotas.js
+++ src/Pages/Rotas/rotas.js
@@ -1,41 +1,64 @@
-import Detalhes from '../Detalhes/Detalhes';
-import Pagamento from '../Pagamento/Pagamento';
-import Principal from '../Principal/Principal';
-import Atualizarcarro from '../Atualizarcarro/Atualizarcarro';
-import FAQ from '../FAQ/index';
-import Contatos from '../Contatos/index';
-import Login from '../Login/Login';
-import Alugar from '../Aluguel';
-import Criarcarro from '../Criarcarro/Criarcarro';
-import{ BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import HistoricoDetalhes from '../Pagamento/historicoDetalhes';
-import Historico from '../Pagamento/historico';
-import Atualizardadoscarro from '../Atualizardadoscarro/Atualizardadoscarro'
+import Detalhes from "../Detalhes/Detalhes";
+import Pagamento from "../Pagamento/Pagamento";
+import Principal from "../Principal/Principal";
+import Atualizarcarro from "../Atualizarcarro/Atualizarcarro";
+import FAQ from "../FAQ/index";
+import Contatos from "../Contatos/index";
+import Login from "../Login/Login";
+import Alugar from "../Aluguel";
+import Criarcarro from "../Criarcarro/Criarcarro";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HistoricoDetalhes from "../Pagamento/historicoDetalhes";
+import Historico from "../Pagamento/historico";
+import Atualizardadoscarro from "../Atualizardadoscarro/Atualizardadoscarro";
+import FormClientes from "../formClientes/formClientes";
 
-
-
-export const AppRouter =() => {
-    return(
-        <Router>
-    <div>
-
-              <Switch>
-              <Route path="/pagamento"><Pagamento /></Route>
-              <Route path={`/detalhes/:id`}><Detalhes /></Route>
-              <Route path="/atualizarcarro"><Atualizarcarro/></Route>
-              <Route path="/FAQ"><FAQ/></Route>
-              <Route path="/contatos"><Contatos/></Route>
-              <Route path="/login"><Login /></Route>
-              <Route path={`/Aluguel/:id`}><Alugar/></Route>
-              <Route path="/criarcarro"><Criarcarro /></Route>
-              <Route path={"/historico/:index"}><HistoricoDetalhes /></Route>
-              <Route path="/historico"><Historico /></Route>
-              <Route path={"/historico/:index"}><Historico /></Route>
-              <Route path="/atualizardadoscarro/:id"><Atualizardadoscarro/></Route>
-              <Route path="/"><Principal /></Route>
-
-              </Switch>
+export const AppRouter = () => {
+  return (
+    <Router>
+  <div>
+        <Switch>
+          <Route path="/pagamento" exact>
+            <Pagamento />
+          </Route>
+          <Route path="/detalhes/:id" exact>
+            <Detalhes />
+          </Route>
+          <Route path="/atualizarcarro" exact>
+            <Atualizarcarro />
+          </Route>
+          <Route path="/FAQ" exact>
+            <FAQ />
+          </Route>
+          <Route path="/contatos" exact>
+            <Contatos />
+          </Route>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
+          <Route path="/aluguel/:id" exact>
+            <Alugar />
+          </Route>
+          <Route path="/criarcarro" exact>
+            <Criarcarro />
+          </Route>
+          <Route path="/historico/:index" exact>
+            <HistoricoDetalhes />
+          </Route>
+          <Route path="/historico" exact>
+            <Historico />
+          </Route>
+          <Route path="/atualizardadoscarro/:id" exact>
+            <Atualizardadoscarro />
+          </Route>
+          <Route path="/clientes" exact>
+            <FormClientes />
+          </Route>
+          <Route path="/" exact>
+            <Principal />
+          </Route>
+        </Switch>
       </div>
-  </Router> 
-    );
-}
+    </Router>
+  );
+};
