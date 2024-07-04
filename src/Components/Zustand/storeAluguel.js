f92328bf59315fef8f9cc82c438bdcab5819e3d9
RafaelRRcruz <164397283+RafaelRRcruz@users.noreply.github.com>
2024-07-04 16:24:45 -0300

    Mudanças

--- src/Components/Zustand/storeAluguel.js
+++ src/Components/Zustand/storeAluguel.js
@@ -1,5 +1,6 @@
-import { create } from 'zustand';
-
+import { create } from "zustand";
+import FordKa from "./../../Assets/FordKa.jpg";
+import FiatUno from "./../../Assets/Fiat-uno21.jpg";
 
 const useAluguelStore = create((set) => ({
   buscar: "",
@@ -10,28 +11,44 @@ const useAluguelStore = create((set) => ({
     nome: "João da Silva",
     valorDiario: null,
     quantDias: 0,
-    formPagamento: "Cartão",
+    formPagamento: "Cartão"
   },
-  setBuscar: (busca) => set({buscar: busca}),
+  setBuscar: (busca) => set({ buscar: busca }),
   setCarroId: (carro) => set({ carroId: carro }),
   setDiasAluguel: (dias) => set({ diasAluguel: dias }),
-  setDados: (novosDados) => set((state) => ({ dados: { ...state.dados, ...novosDados } })),
-  registros: [{
-    carro: 'Ford KA',
-    nome: 'João da Silva',
-    valorDiario: 500,
-    quantDias: 4,
-    formPagamento: 'Pix',
-},
- {
-    carro: 'Ford Fiesta',
-    nome: 'João da Silva',
-    valorDiario: 300,
-    quantDias: 2,
-    formPagamento: 'Cartão',
-},],
-  adicionarRegistro: (novoRegistro) => set((state) => ({ registros: [...state.registros, novoRegistro] })),
+  setDados: (novosDados) =>
+    set((state) => ({ dados: { ...state.dados, ...novosDados } })),
+  registros: [
+    {
+      carro: "Ford KA",
+      nome: "João da Silva",
+      valorDiario: 500,
+      quantDias: 4,
+      formPagamento: "Pix",
+      status: "Alugado",
+      imageUrl: FordKa
+    },
+    {
+      carro: "Fiat Uno",
+      nome: "João da Silva",
+      valorDiario: 300,
+      quantDias: 2,
+      formPagamento: "Cartão",
+      status: "Devolvido",
+      imageUrl: FiatUno
+    }
+  ],
+  adicionarRegistro: (novoRegistro) =>
+    set((state) => ({ registros: [...state.registros, novoRegistro] })),
+  renovarAluguel: (index, dias) =>
+    set((state) => {
+      const updatedRegistros = [...state.registros];
+      updatedRegistros[index] = {
+        ...updatedRegistros[index],
+        quantDias: updatedRegistros[index].quantDias + dias
+      };
+      return { registros: updatedRegistros };
+    })
 }));
-
 
 export default useAluguelStore;
