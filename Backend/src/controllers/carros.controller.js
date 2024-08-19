6d0efff0084621393d984b45fd6cdda86c30541f
Leonardo Yocogawa <leonardoyocogawa@gmail.com>
2024-08-19 00:43:55 -0300

    Paginação de dados (Resultado de pesquisa de carros) e personalizacao de atributos de user dentro de carros

--- Backend/src/controllers/carros.controller.js
+++ Backend/src/controllers/carros.controller.js
@@ -1,4 +1,4 @@
-import {createService, findAllService} from "../services/carros.service.js";
+import {createService, findAllService, countCarros} from "../services/carros.service.js";
 import {ObjectId} from "mongoose";
 
 const create = async (req, res) => {
@@ -28,11 +28,58 @@ const create = async (req, res) => {
 }
 
 const findAll = async (req, res) => {
-    const carros = await findAllService();
+    let {limit, offset} = req.query;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if(!limit){
+        limit = 5;
+    }
+
+    if(!offset){
+        offset = 0;
+    }
+
+    const carros = await findAllService(offset, limit);
+    const total = await countCarros();
+    const currentUrl = req.baseUrl
+
+    const next = offset + limit;
+    const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    //Talvez isso nao seja usado
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
+    //ate aqui talvez nao seja usado
+
     if (carros.length == 0) {
         return res.status(400).send({ message: "Não há carros cadastrados." });
     }
-    res.send(carros);
+
+    //Faz o envio da url com limit e offset para paginacao de dados e uma especie de personalizacao dos atributos do user que criou o carro 
+    res.send({
+        nextUrl,
+        previousUrl,
+        limit,
+        offset,
+        total,
+
+        results: carros.map((carrosItem) => ({
+            id: carrosItem._id,
+            modelo: carrosItem.modelo,
+            ano: carrosItem.ano,
+            cidade: carrosItem.cidade, 
+            precoPorDia: carrosItem.precoPordia,
+            detalhes: carrosItem.detalhes,
+            fotoLink1: carrosItem.fotoLink1,
+            diasAlugado: carrosItem.diasAlugado,
+            dataCriado: carrosItem.dataCriado,
+            userName: carrosItem.user.name,
+            // userEndereco: carrosItem.user.endereco,
+            userEmail: carrosItem.user.email,
+        })),
+    });
 };
 
 export { create, findAll };
--- Backend/src/services/carros.service.js
+++ Backend/src/services/carros.service.js
@@ -2,6 +2,12 @@ import Carros from "../models/Carros.js"
 
 const createService = (body) => Carros.create(body);
 
-const findAllService = () => Carros.find();
+const findAllService = (offset, limit) => Carros.find().sort({_id: -1}).skip(offset).limit(limit).populate("user");
+//Sort ordena por meio de ID. -1 indica que está ordenando a partir do ultimo carro criado
+//offset seta o inicio da busca no banco de dados
+//limit define a intercalacao. Nesse caso, intercala a cada 5 carros no banco de dados
+//populate busca o id de user que criou carro para invocar todos os argumentos de user dentro do carro, como nome, endereco, telefone, para ser usado em cards de resultado de pesquisa e pagina de detalhes do carro, por exemplo
 
-export {createService, findAllService};
+const countCarros = () => Carros.countDocuments();
+
+export {createService, findAllService, countCarros};
