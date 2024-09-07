b8cbf69d63dd491fc0ba75c06d29d503b6012679
Leonardo Yocogawa <leonardoyocogawa@gmail.com>
2024-09-07 14:20:28 -0300

    Tela Detalhes do pedido (/Aluguel)

--- Frontend/src/Pages/Aluguel/index.js
+++ Frontend/src/Pages/Aluguel/index.js
@@ -4,12 +4,21 @@ import Footer from "../../Components/Footer/footer";
 import { Link, useParams, useHistory } from "react-router-dom";
 import carros from "../../Components/Carros/carros";
 import "./aluguel.css";
+import { findCarroById } from '../Services/carrosServices';
+import useAluguelStore from '../../Components/Zustand/storeAluguel.js';
+import format from 'date-fns/format';
 
 export default function Alugar() {
     const { id } = useParams();
     const [carro, setCarro] = useState();
-    const [diasAluguel, setDiasAluguel] = useState(1);
+    //const [diasAluguel, setDiasAluguel] = useState(0);
     const history = useHistory();
+
+    //Zustand
+    const diasEntreDatas = useAluguelStore((state) => state?.diasAluguel);
+    console.log("diasEntreDatas", diasEntreDatas);
+    let primeiroDia = diasEntreDatas?.length > 0 ? format(diasEntreDatas[0], "dd/MM/yyyy") : '';
+    let ultimoDia = diasEntreDatas?.length > 0 ? format(diasEntreDatas[diasEntreDatas.length - 1], "dd/MM/yyyy") : '';
   
     useEffect(() => {
       const carroOfList = carros[id];
@@ -17,12 +26,25 @@ export default function Alugar() {
     }, [id]);
   
     const total = diasAluguel * (carro?.preco || 0); 
+
+    useEffect(() => {
+      const fetchCarrosById = async (id) => {
+          try{
+              const response = await findCarroById(id);
+              console.log("Carro encontrado:", response.data.carros);
+              setCarro(response.data.carros);
+          }catch(error){
+              console.error("Erro ao buscar carros:", error.response.data.message);
+          }
+      };
+      fetchCarrosById(id);
+    }, [])
   
     const handleConfirmar = () => {
       history.push({
         pathname: `/pagamento/${id}`,
         state: { 
-          quantidadeDias: diasAluguel, 
+          quantidadeDias: diasEntreDatas, 
           carro: carro 
         },
       });
@@ -37,32 +59,33 @@ export default function Alugar() {
       </div>
       <div className="containerAluguel">
         <div className="colunm coluna-1">
-          <img src={carro?.Image} alt={`${carro?.modelo} - ${carro?.ano}`} className="img-carro" />
+          <img src={carro?.fotoLink1} alt={`${carro?.modelo} - ${carro?.ano}`} className="img-carro" />
         </div>
         <div className="culunm coluna-2">
-          <p className="item"><b>Proprietário: </b> {carro?.dono}</p>
+          <p className="item"><b>Proprietário: </b> {carro?.userName}</p>
           <p className="item"><b>Modelo: </b> {carro?.modelo} {carro?.ano}</p>
           <p className="item"><b>Cidade: </b> {carro?.cidade}</p>
-          <p className="item"><b>Preço/dia: </b> R$ {carro?.preco.toFixed(2)}</p>
+          <p className="item"><b>Preço/dia: </b> R$ {carro?.precoPorDia}</p>
         </div>
       </div>
 
       <div className="container">
         <div className="diasAluguel">
-          <p className="titulo2">Quantidade de dias para alugar:</p>
-          <input
+          <p className="titulo2">Dias para alugar:</p>
+          {/* <input
             type="number"
-            value={diasAluguel}
+            value={diasEntreDatas}
             onChange={(e) => setDiasAluguel(Math.max(1, e.target.value))}
             min={1}
             className="input-dias"
-          />
+          /> */}
+          <p className="item">{`${diasEntreDatas.length} dias (${primeiroDia} até ${ultimoDia})`}</p>
         </div>
       </div>
 
       <div className="container-preco">
         <div className="precos">
-          <p className="total">Total a pagar: R$ {total.toFixed(2)}</p>
+          <p className="total">Total a pagar: R$ {total}</p>
         </div>
       </div>
 
